683df9b48322db55a2a74874eea5e503797dc020
Amaia Murat <amaia.murat@bison.howard.edu>
2026-09-17 05:00:56 +0000

    Connects client, send message and recieve response.

--- basic_client.py
+++ basic_client.py
@@ -1,4 +1,15 @@
 from socket import * #Imports all socket functions and constants 
 
+#CREATING SOCKET 
+
 serverName = 'localhost' #set the server's hostname to the local computer
 serverPort = 12000 #Port num used by the server
+
+clientSocket = socket(AF_INET, SOCK_STREAM) #Create a IPv4 TCP Socket
+clientSocket.connect((serverName, serverPort)) #Connect client to server
+
+#USER MSG LOGIC
+
+sentence = input('Enter lowercase sentence:') #Ask the user for a msg
+clientSocket.send(sentence.encode())
+modifiedSentence = clientSocket.recv(1024).decode #Recieve and decode the server
