758cf264a00ba9e372f6a36bc97c3e43ac3bed2d
Amaia Murat <amaia.murat@bison.howard.edu>
2026-09-17 04:56:28 +0000

    Created TCP client socket.

---
+++ basic_client.py
@@ -0,0 +1,4 @@
+from socket import * #Imports all socket functions and constants 
+
+serverName = 'localhost' #set the server's hostname to the local computer
+serverPort = 12000 #Port num used by the server
