c3fa78124b67cdb5e1ccad1608aecc94def6c06d
Amaia Murat <amaia.murat@bison.howard.edu>
2026-09-18 01:00:08 +0000

    Further Debugging and Refactoring.

--- basic_client.py
+++ basic_client.py
@@ -2,18 +2,20 @@ from socket import * #Imports all socket functions and constants
 
 #CREATING SOCKET 
 
-serverName = 'localhost' #set the server's hostname to the local computer
-serverPort = 12000 #Port num used by the server
+server_name = 'localhost' #set the server's hostname to the local computer
+server_port = 46767 #Port num used by the server
 
-clientSocket = socket(AF_INET, SOCK_STREAM) #Create a IPv4 TCP Socket
-clientSocket.connect((serverName, serverPort)) #Connect client to server
+client_socket = socket(AF_INET, SOCK_STREAM) #Create a IPv4 TCP Socket
+client_socket.connect((server_name, server_port)) #Connect client to server
 
 #USER MSG LOGIC
 
 sentence = input('Enter lowercase sentence:') #Ask the user for a msg
-clientSocket.send(sentence.encode())
-sentenceModified = clientSocket.recv(1024).decode() #Recieve and decode the server
+client_socket.send(sentence.encode())
+sentence_modified = client_socket.recv(1024).decode() #Recieve and decode the server
 
-print('From Server:', sentenceModified) #Display the server's reponse
+print('FROM SERVER:', sentence_modified) #Display the server's reponse
 
-clientSocket.close()
+client_socket.close()
+
+print("The client socket has closed.")
--- basic_server.py
+++ basic_server.py
@@ -2,26 +2,36 @@ from socket import *
 
 #CREATE AND BIND SOCKET 
 
-serverPort = 12000 #Set the server port num
-serverSocket = socket(AF_INET, SOCK_STREAM) #Create TCP Socket using IPv4 
-serverSocket.bind(('', serverPort)) #Bind the server socket to port num
+server_port = 46767 #Set the server port num
+server_socket = socket(AF_INET, SOCK_STREAM) #Create TCP Socket using IPv4 
+
+print('A server socket has been created.') #Denotes that the server socket has been created
+
+server_socket.bind(('', server_port)) #Bind the server socket to port num
 
 #LISTENING 
 
-serverSocket.listen(1) #Listen for client connections
-print('The server is ready to recieve') #Confirm the server is running
+server_socket.listen(1) #Listen for client connections
+
+print('The server is running and ready to receive.') #Confirm the server is running
 
 while True:
 
-    connectionSocket, addr = serverSocket.accept() #Accept connection from the client
-    sentence = connectionSocket.recv(1024).decode() #Recieve and decode the client msg
+    connection_socket, addr = server_socket.accept() #Accept connection from the client
 
+    print("The server socket has connected to the client.") #Confirm client connection
+
+    sentence = connection_socket.recv(1024).decode() #Recieve and decode the client msg
+
+    print('FROM CLIENT:', sentence) #Display client msg
     #FORMATTING
 
-    sentenceCapitalized = sentence.upper()
+    response = "Your message has been received, thank you!"
 
     #SEND RESPONSE MSG
 
-    connectionSocket.send(sentenceCapitalized.encode()) #Encode and send response to client
+    connection_socket.send(response.encode()) #Encode and send response to client
 
-    connectionSocket.close()
+    connection_socket.close()
+
+    print("The server socket has closed.")
